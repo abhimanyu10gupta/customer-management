411232dd54137d48709b24ad7717ccdf180fc706
Aabhimanyu Gupta <abhimanyu10gupta@github.com>
2020-04-14 15:39:29 +1000

    initialised templates and static files

---
+++ accounts/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def home(request):
+    return render(request, 'accounts/dashboard.html')
+def product(request):
+    return render(request, 'accounts/products.html')
+
+def customers(request):
+    return render(request, 'accounts/customers.html')
