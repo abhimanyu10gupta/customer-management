095bad5e303e35d8f8eacf99a9354de505db80a5
Aabhimanyu Gupta <abhimanyu10gupta@github.com>
2020-04-15 13:02:18 +1000

    add CRUD

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,8 +4,9 @@ from . import views
 urlpatterns = [
     path('', views.home, name="home"),
     path('product/', views.product, name="products"),
-    path('customer/<str:pk>', views.customers, name="customer"),
-
-
+    path('customer/<str:pk>/', views.customers, name="customer"),
+    path('create_order', views.createOrder, name="create_order"),
+    path('update_order/<str:pk>/', views.updateOrder, name="update_order"),
+    path('delete_order/<str:pk>/', views.deleteOrder, name="delete_order"),
 
 ]
